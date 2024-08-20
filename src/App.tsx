f41a235ff5b2285bc7ab7fa5c5c2a9551dd7905d
mikieoo <jcw2551@naver.com>
2024-08-20 12:13:18 +0900

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import Component, { FunctionComponent } from './component/Component';
+// import Component, { FunctionComponent } from './component/Component';
 import './App.css';
-import CurlyBraces from './component/CurlyBraces';
+// import CurlyBraces from './component/CurlyBraces';
+import Properties from './component_manage/Properties';
+import ConditionalRender from './component_manage/ConditionalRender';
 
 function App() {
   return (
     <>
       {/* {Component} */}
       {/* {FunctionComponent} */}
-      <CurlyBraces />
-    </>
-    
+      {/* <CurlyBraces /> */}
+      {/* <Properties /> */}
+      <ConditionalRender />
+    </> 
   );
 }
 
