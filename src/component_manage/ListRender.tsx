a4c53501b5fbe6aef4619554205d3360b5cbe516
mikieoo <jcw2551@naver.com>
2024-08-21 10:23:22 +0900

    first

---
+++ src/component_manage/ListRender.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// 리스트 렌더링 : 
+// - JSX 내부에서 반복적으로 렌더링하는 요소에 대하여 반복 처리하는 방법
+// - JSX 내부에서는 for, while 같은 반복 구문을 사용할 수 없음
+// - 리스트의 각종 메서드 활용
+
+export default function ListRender() {
+    return (
+        <div>ListRender</div>
+    )
+}
---
+++ src/component_manage/example/Example2.tsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import './Example2.css';
+
+function TableHeader() {
+    return (
+        <div className='th'>
+            <div className='td'>사번</div>
+            <div className='td'>이름</div>
+            <div className='td'>출근상태</div>
+        </div>
+    );
+}
+
+interface TableRowProps {
+    employeeNumber: string;
+    employeeName: string;
+    workState: boolean;
+}
+
+function TableRow({ employeeNumber, employeeName, workState }: TableRowProps) {
+
+    const color = workState ? 'blue' : 'red';
+
+    return (
+        <div className='tr'>
+            <div className='td'>{employeeNumber}</div>
+            <div className='td'>{employeeName}</div>
+            <div className='td' style={{ color }}>●</div>
+        </div>
+    )
+}
+
+//=========================================================================================
+
+interface TableDataTrProps {
+    type: 'tr'
+    employeeNumber: string;
+    employeeName: string;
+    workState: boolean;
+}
+
+interface TableDataThProps {
+    type: 'th'
+}
+
+function TableData(props: TableDataTrProps | TableDataThProps) {
+
+    const { type } = props; // 공통으로 꺼내올수 있는 요소만 가져올 수 있음
+
+    if (type === 'th') {
+        return (
+            <div className='th'>
+                <div className='td'>사번</div>
+                <div className='td'>이름</div>
+                <div className='td'>출근상태</div>
+            </div>
+        )
+    }
+
+    const { employeeNumber, employeeName, workState } = props;
+    const color = workState ? 'blue' : 'red';
+
+    return (
+        <div className='tr'>
+            <div className='td'>{employeeNumber}</div>
+            <div className='td'>{employeeName}</div>
+            <div className='td' style={{ color }}>●</div>
+        </div>
+    );
+}
+
+export default function Example2() {
+
+    const employee1 = {
+        employeeNumber: '202401',
+        employeeName: '홍길동',
+        workState: false
+    };
+
+    const employee2 = {
+        employeeNumber: '202402',
+        employeeName: '이영희',
+        workState: true
+    };
+
+    const employee3 = {
+        employeeNumber: '202403',
+        employeeName: '김철수',
+        workState: true
+    };
+
+    return (
+        <div className='table'>
+            {/* <TableHeader />
+            <TableRow {...employee1} />
+            <TableRow {...employee2} />
+            <TableRow {...employee3} /> */}
+
+            {/* <TableData type='th' />
+            <TableData type='tr' {...employee1} />
+            <TableData type='tr' {...employee2} />
+            <TableData type='tr' {...employee3} /> */}
+
+            {/* <TableHeader />
+            <TableRow {...employees[0]} />
+            <TableRow {...employees[1]} />
+            <TableRow {...employees[2]} /> */}
+        </div>
+    );
+}
+
+const employees = [
+    {
+        employeeNumber: '202401',
+        employeeName: '홍길동',
+        workState: false
+    },
+
+    {
+        employeeNumber: '202402',
+        employeeName: '이영희',
+        workState: true
+    },
+
+    {
+        employeeNumber: '202403',
+        employeeName: '김철수',
+        workState: true
+    }
+]
