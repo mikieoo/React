e51b0b4cdc6dbe202cdc33f53d24f25d062fc4fc
mikieoo <jcw2551@naver.com>
2024-08-22 12:31:35 +0900

    first

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 // import Component, { FunctionComponent } from './component/Component';
 import './App.css';
 // import CurlyBraces from './component/CurlyBraces';
-import Properties from './component_manage/Properties';
-import ConditionalRender from './component_manage/ConditionalRender';
-import Example2 from './component_manage/example/Example2';
-import ListRender from './component_manage/ListRender';
+// import Properties from './component_manage/Properties';
+// import ConditionalRender from './component_manage/ConditionalRender';
+// import Example2 from './component_manage/example/Example2';
+// import ListRender from './component_manage/ListRender';
+import EventComponent from './interaction/EventComponent';
+import StateComponent from './interaction/StateComponent';
 
 function App() {
   return (
@@ -15,8 +17,10 @@ function App() {
       {/* <CurlyBraces /> */}
       {/* <Properties /> */}
       {/* <ConditionalRender /> */}
-      <Example2 />
+      {/* <Example2 /> */}
       {/* <ListRender /> */}
+      {/* <EventComponent /> */}
+      <StateComponent />
     </> 
   );
 }
